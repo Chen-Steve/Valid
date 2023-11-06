8ea64cf7697064deb8a1cb69754a4ce0c588f385
Steven <zhangc2@illinois.edu>
2023-11-06 01:18:14 -0600

    initial valid

---
+++ src/components/header.tsx
@@ -0,0 +1,36 @@
+// pages/index.tsx
+import React from 'react';
+import Head from 'next/head';
+import styles from '../styles/header.module.css';
+import Link from 'next/link';
+
+const Header: React.FC = () => {
+    return (
+        <>
+        <Head>
+            <title>Welcome Page</title>
+            <link
+                href="https://fonts.googleapis.com/css2?family=Jua&display=swap"
+                rel="stylesheet"
+                />
+            </Head>
+          <div className={styles.container}>
+            <div className={`${styles.circle} ${styles.circle1}`} />
+            <div className={`${styles.circle} ${styles.circle2}`} />
+            <div className={`${styles.circle} ${styles.circle3}`} />
+            <div className={`${styles.circle} ${styles.circle4}`} />
+            <div className={`${styles.circle} ${styles.circle5}`} />
+            <div className={`${styles.circle} ${styles.circle6}`} />
+            <div className={`${styles.circle} ${styles.circle7}`} />
+            <div className={`${styles.circle} ${styles.circle8}`} />
+            <p className={styles.loginLink}>
+                <Link href="/login">Login</Link>
+            </p>
+            <h1 className={styles.title}>Welcome :)</h1>
+          </div>
+        </>
+      );
+};
+
+export default Header;
+
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import styles from '../app/page.module.css'
+import '../app/globals.css'
+import Header from '../components/header';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Home
